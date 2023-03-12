51bda93e3987954644e7a74e933172711be9f6d9
Raul <krolik-0304@bk.ru>
2023-03-12 19:29:50 +0600

    Initial Commit

---
+++ W 7 m3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W 7 m3
+//
+//  Created by Рауль on 12/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
